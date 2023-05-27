10a783fe756cc3fb53f1d7a436eed24c05f6a7f6
Sebastian Celsi <sebastian.celsi@gmail.com>
2023-05-27 06:46:00 -0300

    Modified phone.js

---
+++ scripts/phone.js
@@ -0,0 +1,13 @@
+Swal.fire({
+  title: "Mensaje de número desconocido",
+  text: "Deseas agregar como contacto?",
+  icon: "warning",
+  width: "50%",
+  backdrop: "true",
+  timer: "6000",
+  timerProgressBar: "true",
+  position: "center",
+  showConfirmButtonText: "true",
+  showCancelButton: "true",
+  showCloseButton: "true",
+});
