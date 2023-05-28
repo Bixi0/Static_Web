ba3191529e33abbc358e5dcd3f434e39321b67bd
Sebastian Celsi <sebastian.celsi@gmail.com>
2023-05-28 11:22:22 -0300

    adding popup functions

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 //Alert Message!!//
 
-Swal.fire({
+/*Swal.fire({
   title: "Mensaje de número desconocido",
   text: "Deseas agregar como contacto?",
   icon: "warning",
@@ -12,4 +12,51 @@ Swal.fire({
   showConfirmButtonText: "true",
   showCancelButton: "true",
   showCloseButton: "true",
+});*/
+
+//Capture current time//
+
+const time = document.getElementById("clock");
+
+const interval = setInterval(() => {
+  const currentTime = new Date();
+
+  time.innerHTML = currentTime.toLocaleTimeString();
+}, 1000);
+
+//Alert Message!!//
+
+document.addEventListener("DOMContentLoaded", function () {
+  var popup = document.getElementById("popup");
+
+  if (!sessionStorage.getItem("popupShown")) {
+    popup.classList.remove("hidden");
+    sessionStorage.setItem("popupShown", "true");
+  }
+
+  popup.addEventListener("animationend", function () {
+    popup.classList.add("hidden");
+  });
 });
+
+const mostrarVentanaEmergente = () => {
+  var popup = document.getElementById("popup");
+  popup.style.display = "block";
+  setTimeout(
+    (mostrarVentanaEmergente = () => {
+      popup.style.display = "none";
+    }),
+    5000
+  ); // Cerrar la ventana emergente después de 5 segundos (5000 milisegundos)
+};
+
+const aceptar = () => {
+  var popup = document.getElementById("popup");
+  popup.style.display = "none";
+  // Acá se puede agregar el código que se ejecutará cuando el usuario haga clic en "Aceptar"
+};
+
+const cancelar = () => {
+  var popup = document.getElementById("popup");
+  popup.style.display = "none";
+};
