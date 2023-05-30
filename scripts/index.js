6bc6fcb4cc64e0f4dfd80caac187a91fc2c9bc74
Sebastian Celsi <sebastian.celsi@gmail.com>
2023-05-30 18:46:36 -0300

    modified index.js

--- scripts/index.js
+++ scripts/index.js
@@ -15,53 +15,53 @@
 });*/
 
 // Obtener la hora actual sin segundos
-var date = new Date();
-var hours = date.getHours();
-var minutes = date.getMinutes();
+const date = new Date();
+const hours = date.getHours();
+const minutes = date.getMinutes();
 
 // Formatear la hora sin segundos
-var time = hours + ":" + (minutes < 10 ? "0" : "") + minutes;
+const time = hours + ":" + (minutes < 10 ? "0" : "") + minutes;
 
 // Mostrar la hora en el elemento correspondiente
-var timeElement = document.getElementById("clock");
+const timeElement = document.getElementById("clock");
 timeElement.textContent = time;
 
 //Alert Message!!//
 
-window.addEventListener("load", function () {
-  var popup = document.getElementById("popup");
-  var showTimeout;
+window.addEventListener("load", () => {
+  const popup = document.getElementById("popup");
+  showTimeout;
 
   // Función para mostrar la ventana emergente después de un retraso
-  function showPopup() {
+  const showPopup = () => {
     popup.style.display = "block";
-  }
+  };
 
   // Mostrar la ventana emergente después de 3 segundos (3000 milisegundos)
   showTimeout = setTimeout(showPopup, 3000);
 
   // Función para cerrar la ventana emergente
-  function closePopup() {
+  const closePopup = () => {
     popup.style.display = "none";
-  }
+  };
 
   // Función para manejar el clic en el botón "Aceptar"
-  function accept() {
+  const accept = () => {
     closePopup();
     clearTimeout(showTimeout); // Cancelar el timeout existente
     // Redirigir a otro archivo HTML
     window.location.href = "/pages/wsp_trucho.html";
-  }
+  };
 
   // Función para manejar el clic en el botón "Cancelar"
-  function cancel() {
+  const cancel = () => {
     closePopup();
     showTimeout = setTimeout(showPopup, 2000); // Volver a mostrar la ventana después de 5 segundos (5000 milisegundos)
-  }
+  };
 
   // Agregar eventos a los botones
-  var acceptButton = popup.querySelector(".buttons button:first-of-type");
-  var cancelButton = popup.querySelector(".buttons button:last-of-type");
+  const acceptButton = popup.querySelector(".buttons button:first-of-type");
+  const cancelButton = popup.querySelector(".buttons button:last-of-type");
   acceptButton.addEventListener("click", accept);
   cancelButton.addEventListener("click", cancel);
 });
